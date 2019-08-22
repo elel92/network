ca3e5e07b52f67b36e77ba76ba6b523bbd5443f8
BIT <BIT@DESKTOP-Q04GE57>
2019-08-22 16:01:34 +0900

    NSLookup 완료

--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -2,22 +2,30 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
 		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.naver.com");
-			
-			for(InetAddress inetAddress : inetAddresses) {
-				System.out.println(inetAddress.getHostAddress());
+			while(true) {
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.print("> ");
+				String dns = sc.nextLine();
+				
+				if(dns.equals("exit")) {
+					break;
+				}
+				
+				InetAddress[] inetAddresses = InetAddress.getAllByName(dns);
+				
+				for(InetAddress inetAddress : inetAddresses) {
+					System.out.println(dns + " : " +inetAddress.getHostAddress());
+				}
 			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		
-		
-
 	}
-
 }
